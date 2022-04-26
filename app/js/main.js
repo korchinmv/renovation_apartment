241f6d28f0bd36966d4c94e3ee6afbcc01bf5992
korchinmv <korchinmv@yandex.ru>
2022-04-26 09:13:20 +0300

    first

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,7 @@
-$(function () {
+// const buttonMenu = document.querySelector('.burger');
+// const menuActive = document.querySelector('.menu');
 
-
-});
+// buttonMenu.onclick = function () {
+// 	menuActive.classList.toggle('menu--active');
+// 	buttonMenu.classList.toggle('burger--active');
+// };
